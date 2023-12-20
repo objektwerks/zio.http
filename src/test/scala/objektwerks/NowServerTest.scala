73e3d62e72deda65d88165c59f2159ade1b5881a
objektwerks <objektwerks@runbox.com>
2023-12-20 08:34:17 -0500

    corrected now server test

--- src/test/scala/objektwerks/NowServerTest.scala
+++ src/test/scala/objektwerks/NowServerTest.scala
@@ -1,14 +1,15 @@
 package objektwerks
 
-import zio.test.*
-import zio.test.Assertion.equalTo
 import zio.http.*
+import zio.test.*
 
 object NowServerTest extends ZIOSpecDefault:
   def spec = suite("http")(
     test("now server should return ok") {
       val routes = NowServer.routes
       val request = Request.get("http://localhost:7070/now")
-      assertZIO(routes.runZIO(request))(equalTo(Response.ok))
+      for
+        response <- routes.runZIO(request)
+      yield assertTrue( response.status == Status.Ok )
     }
   )
