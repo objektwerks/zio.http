c5099d3c9a25c2121016bddb16ee198bbffa9aed
objektwerks <objektwerks@runbox.com>
2023-12-21 11:12:22 -0500

    enhanced command json support

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -6,3 +6,4 @@ final case class Command(name: String)
 
 object Command:
   given JsonDecoder[Command] = DeriveJsonDecoder.gen[Command]
+  given JsonEncoder[Command] = DeriveJsonEncoder.gen[Command]
