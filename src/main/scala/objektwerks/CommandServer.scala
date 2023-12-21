1a81d8f302c5df4cbfb5a47fe3fb5e64ccb52e9b
objektwerks <objektwerks@runbox.com>
2023-12-21 11:57:59 -0500

    edited command server

--- src/main/scala/objektwerks/CommandServer.scala
+++ src/main/scala/objektwerks/CommandServer.scala
@@ -8,6 +8,10 @@ import Command.given
 import Event.given
 
 object CommandServer extends ZIOAppDefault:
+  val commandHandler = Handler
+    .fromFunction[Command] { case Command(name) => Event(name) }
+    .map(event => Response.json(event.toJson))
+    
   val routes = Routes(
     Method.POST / "command" -> handler { (request: Request) =>
       (
