c878ad90e0e57abb1fe166e1fe2567b56c26dda4
objektwerks <objektwerks@runbox.com>
2023-12-21 11:03:46 -0500

    enhanced command server

--- src/main/scala/objektwerks/CommandServer.scala
+++ src/main/scala/objektwerks/CommandServer.scala
@@ -5,15 +5,20 @@ import zio.http.*
 import zio.json.*
 
 import Event.given
+import scala.util.control.NonFatal
 
 object CommandServer extends ZIOAppDefault:
   val routes = Routes(
     Method.POST / "command" -> handler { (request: Request) =>
-      for
-        json    <- request.body.asString
-        command <- json.fromJson[Command]
-        event    = Event(name = command.name)
-      yield Response.json(event.toJson)
+      (
+        for
+          json    <- request.body.asString
+          command <- json.fromJson[Command]
+          event    = Event(name = command.name)
+        yield Response.json(event.toJson)
+      ).recover {
+        case NonFatal(error) => s"{error: ${error.getMessage}}"
+      }
     }
   ).toHttpApp
 
