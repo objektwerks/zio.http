098a063167e855abcf3222524afe226a4554419a
objektwerks <objektwerks@runbox.com>
2024-07-30 13:06:11 -0400

    enhanced command server test

--- src/test/scala/objektwerks/CommandServerTest.scala
+++ src/test/scala/objektwerks/CommandServerTest.scala
@@ -1,10 +1,12 @@
 package objektwerks
 
+import zio.*
 import zio.http.*
 import zio.json.*
 import zio.test.*
 
 import Command.given
+import Event.given
 
 object CommandServerTest extends ZIOSpecDefault:
   def spec = suite("http")(
@@ -12,6 +14,11 @@ object CommandServerTest extends ZIOSpecDefault:
       val request = Request.post("http://localhost:6060/command", Body.fromString( Command("test").toJson ) )
       for
         response <- CommandServer.routes.runZIO(request)
-      yield assertTrue( response.status == Status.Ok )
+      yield
+        for
+          body  <- response.body.asString.orDie
+          event <- ZIO.fromEither( body.fromJson[Event] )
+        yield assertTrue( event.name == "test")
+        assertTrue( response.status == Status.Ok )
     }
   )
