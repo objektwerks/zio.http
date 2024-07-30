f5166c02873c0e440d139d3fd1be64c09b577a6d
objektwerks <objektwerks@runbox.com>
2024-07-30 12:24:21 -0400

    formatted command server

--- src/main/scala/objektwerks/CommandServer.scala
+++ src/main/scala/objektwerks/CommandServer.scala
@@ -9,13 +9,12 @@ import Event.given
 
 object CommandServer extends ZIOAppDefault:
   val routes = Routes(
-    Method.POST / "command" -> handler { (request: Request) =>
-      for {
+    Method.POST / "command" -> handler: (request: Request) =>
+      for
         body    <- request.body.asString.orDie
         command <- ZIO.fromEither( body.fromJson[Command] )
         event   = Event( command.name ).toJson
-      } yield Response.json(event)
-    }
+      yield Response.json(event)
   ).handleError(_ match
     case _: String => Response.badRequest("Invalid json.")
   )
