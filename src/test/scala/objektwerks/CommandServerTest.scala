eca072c19d1651c0ab1c59cec8849a62bde9a34d
objektwerks <objektwerks@runbox.com>
2024-07-30 11:31:34 -0400

    edited command server test

--- src/test/scala/objektwerks/CommandServerTest.scala
+++ src/test/scala/objektwerks/CommandServerTest.scala
@@ -6,11 +6,9 @@ import zio.test.*
 object CommandServerTest extends ZIOSpecDefault:
   def spec = suite("http")(
     test("command server should return ok") {
-      val routes = CommandServer.routes
       val request = Request.get("http://localhost:6060/command")
-      val app = routes.toHttpApp
       for
-        response <- app.runZIO(request)
+        response <- CommandServer.app.runZIO(request)
       yield assertTrue( response.status == Status.Ok )
     }
   )
