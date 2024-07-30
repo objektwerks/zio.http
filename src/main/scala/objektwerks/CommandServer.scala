89c31ae01865b7edc1e430a1798b427ce2e5341c
objektwerks <objektwerks@runbox.com>
2024-07-30 10:19:10 -0400

    edited command server again

--- src/main/scala/objektwerks/CommandServer.scala
+++ src/main/scala/objektwerks/CommandServer.scala
@@ -8,16 +8,16 @@ import Command.given
 import Event.given
 
 object CommandServer extends ZIOAppDefault:
-  val handler = Handler.fromFunction[Command] { case Command(name) => Event(name) }
+  val commandHandler = Handler.fromFunction[Command] { case Command(name) => Event(name) }
 
   val routes = Routes(
-    Method.POST / "command" -> handler
+    Method.POST / "command" -> commandHandler
       // Error: Found: objektwerks.Command Required: zio.ZIO[Nothing, Any, Any]
       // Using any ZIO.* constructor method results in the opposite error: objektwerks.Command required
       .contramap[Request](request => request.body.asString.flatMap { json => json.fromJson[Command] } )
       .map(event => Response.json(event.toJson))
   ).toHttpApp
 
-  override val run = Server
+  def run = Server
     .serve(routes)
     .provide(Server.defaultWithPort(6060))
