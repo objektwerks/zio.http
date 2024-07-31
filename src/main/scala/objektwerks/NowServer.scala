0150336661e97cd9c901700ac358e17982276a90
objektwerks <objektwerks@runbox.com>
2024-07-31 17:29:38 -0400

    enhanced now server

--- src/main/scala/objektwerks/NowServer.scala
+++ src/main/scala/objektwerks/NowServer.scala
@@ -6,7 +6,7 @@ import zio.*
 import zio.http.*
 
 object NowServer extends ZIOAppDefault:
-  val routes = Routes(
+  val routes: Routes[Any, Response] = Routes(
     Method.GET / "now" -> handler { (_: Request) =>
       Response.text( s"*** Now: ${Instant.now.toString}" )
     }
